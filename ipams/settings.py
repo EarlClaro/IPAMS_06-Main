5fa7ddc9003902eca176583f764ed0419b8d08a9
EarlClaro <earlclaro@gmail.com>
2024-05-14 18:29:09 +0800

    update

--- ipams/settings.py
+++ ipams/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ipamsdjango',
         'USER': 'root',
-        'PASSWORD': 'Moplex-storm123',
+        'PASSWORD': 'new_password',
         'PORT': '3306',
         'HOST': 'localhost',
         'OPTIONS': {
@@ -121,8 +121,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nalc_schema',
         'USER': 'root',  # Replace with the appropriate username
-        'PASSWORD': '123456',  # Replace with the appropriate password
-        'PORT': '3307',
+        'PASSWORD': 'new_password',  # Replace with the appropriate password
+        'PORT': '3306',
         'HOST': 'localhost',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
