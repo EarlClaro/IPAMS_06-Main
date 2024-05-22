da7fa47b81d7254ffaeff0634b3593222eea3839
EarlClaro <earlclaro@gmail.com>
2024-05-22 19:20:43 +0800

    update

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-# ipams_capstone2/wsgi.py
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipams_capstone2.settings')
-
-application = get_wsgi_application()
