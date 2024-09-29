8c86e37f8dbc9f61cc7384bb897a4f2f1bc731a4
John Mc Lient <121336572+badblood1232@users.noreply.github.com>
2024-09-29 23:28:31 +0900

    getsession

--- records/context_processors.py
+++ records/context_processors.py
@@ -4,11 +4,28 @@ from .models import Subscription
 def subscription_status(request):
     if request.user.is_authenticated:
         subscription = request.user.get_subscription()
-        show_modal = subscription.is_near_end() and subscription.plan_id_id in [2, 3] if subscription else False
+        show_modal = False
+       
         if subscription:
             # Use locked_plan_name to show the subscribed plan, not the updated one
             plan_name = subscription.locked_plan_name if subscription.locked_plan_name else 'Free'
+
+            if subscription.is_near_end() and subscription.plan_id_id in [2, 3]:
+               if not request.session.get('modal_shown', False):
+                   show_modal = True
+                   request.session['modal_shown'] = True
         else:
             plan_name = 'Free'
         return {'show_modal': show_modal, 'subscription_plan_name': plan_name}
     return {'show_modal': False, 'subscription_plan_name': 'Free'}
+
+
+
+
+
+
+
+
+
+
+
